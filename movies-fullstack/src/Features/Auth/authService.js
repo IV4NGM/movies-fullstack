3679f3558703a8e8fb9d591a86b43803e1eb4811
IvanGM <ivangm_01@hotmail.com>
2024-03-15 02:51:20 -0600

    feat: Añadida funcionalidad de auth completa

--- movies-fullstack/src/Features/Auth/authService.js
+++ movies-fullstack/src/Features/Auth/authService.js
@@ -1,7 +1,100 @@
 import axios from 'axios'
 
-const authService = {
+const API_URL = import.meta.env.VITE_API_URL + 'users/'
 
+// Creamos la petición al backend para crear un usuario
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData)
+
+  return response.data
+}
+
+// Creamos la petición al backend para login usuario
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
+// Logout a un usuario
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+// Petición de enviar email de verificación
+const sendVerificationEmail = async (email) => {
+  const response = await axios.post(API_URL + 'send-verification-email', { email })
+
+  return response.data
+}
+
+// Petición de verificar usuario
+// userData = {id, token}
+const verifyUser = async (userData) => {
+  const response = await axios.post(API_URL + 'verify', userData)
+
+  return response.data
+}
+
+// Petición de restablecer contraseña olvidada
+const sendResetEmail = async (email) => {
+  const response = await axios.post(API_URL + 'send-reset-email', { email })
+
+  return response.data
+}
+
+// Restablecer contraseña olvidada
+// userData = {id, token, password}
+const resetPassword = async (userData) => {
+  const response = await axios.post(API_URL + 'reset-password', userData)
+
+  return response.data
+}
+
+// Modificar usuario
+const updateUser = async (userData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const response = await axios.put(API_URL, userData, config)
+
+  return response.data
+}
+
+// Eliminar usuario
+const deleteUser = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const response = await axios.delete(API_URL, config)
+
+  if (response.status === 200) {
+    logout()
+  }
+
+  return response.data
+}
+
+const authService = {
+  register,
+  login,
+  logout,
+  sendVerificationEmail,
+  verifyUser,
+  sendResetEmail,
+  resetPassword,
+  updateUser,
+  deleteUser
 }
 
 export default authService
--- movies-fullstack/src/Features/Auth/authSlice.js
+++ movies-fullstack/src/Features/Auth/authSlice.js
@@ -12,6 +12,92 @@ const initialState = {
   message: ''
 }
 
+// Registrar un nuevo usuario
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+  try {
+    return await authService.register(user)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Login un usuario
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  try {
+    return await authService.login(user)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Logout
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
+
+// Petición de enviar email de verificación
+export const sendVerificationEmail = createAsyncThunk('auth/send-verification-email', async (email, thunkAPI) => {
+  try {
+    return await authService.sendVerificationEmail(email)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Petición de verificar usuario
+export const verifyUser = createAsyncThunk('auth/verify', async (userData, thunkAPI) => {
+  try {
+    return await authService.verifyUser(userData)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Petición de restablecer contraseña olvidada
+export const sendResetEmail = createAsyncThunk('auth/send-reset-email', async (email, thunkAPI) => {
+  try {
+    return await authService.sendResetEmail(email)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Restablecer contraseña olvidada
+export const resetPassword = createAsyncThunk('auth/reset-password', async (userData, thunkAPI) => {
+  try {
+    return await authService.resetPassword(userData)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Modificar usuario
+export const updateUser = createAsyncThunk('auth/update-user', async (userData, thunkAPI) => {
+  try {
+    return await authService.updateUser(userData)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Eliminar usuario
+// thunkAPI.getState().auth.user.token
+export const deleteUser = createAsyncThunk('auth/delete-user', async (_, thunkAPI) => {
+  try {
+    return await authService.deleteUser(thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -24,6 +110,108 @@ export const authSlice = createSlice({
     }
   },
   extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
+      })
+      .addCase(sendVerificationEmail.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(sendVerificationEmail.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(sendVerificationEmail.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(verifyUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(verifyUser.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(verifyUser.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(sendResetEmail.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(sendResetEmail.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(sendResetEmail.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(resetPassword.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(resetPassword.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(resetPassword.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(deleteUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = null
+      })
+      .addCase(deleteUser.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
   }
 })
 
