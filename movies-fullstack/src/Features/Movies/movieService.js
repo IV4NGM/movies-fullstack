faf1c24db0b18037df6f856827b62b68159355ca
IvanGM <ivangm_01@hotmail.com>
2024-03-15 15:49:53 -0600

    feat: Agregados servicios de películas

--- movies-fullstack/src/Features/Movies/movieService.js
+++ movies-fullstack/src/Features/Movies/movieService.js
@@ -1,7 +1,117 @@
 import axios from 'axios'
 
-const movieService = {
+const API_URL = import.meta.env.VITE_API_URL + 'movies/'
 
+// Obtener todos los géneros
+const getAllGenres = async () => {
+  const response = await axios.get(API_URL + 'genres')
+
+  return response.data
+}
+
+// Obtener todas las películas (not logged)
+const getAllMovies = async () => {
+  const response = await axios.get(API_URL)
+
+  return response.data
+}
+
+// Obtener todas las películas (logged)
+const getContextMovies = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const response = await axios.get(API_URL + 'all', config)
+
+  return response.data
+}
+
+// Like movie
+const likeMovie = async (movieId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  const response = await axios.get(API_URL + `like/${movieId}`, config)
+
+  return response.data
+}
+
+// Dislike movie
+const dislikeMovie = async (movieId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  const response = await axios.get(API_URL + `dislike/${movieId}`, config)
+
+  return response.data
+}
+
+// Reset likes/dislikes
+const resetLikesMovie = async (movieId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  const response = await axios.get(API_URL + `reset-likes/${movieId}`, config)
+
+  return response.data
+}
+
+// Crear película
+const createMovie = async (movieData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  const response = await axios.post(API_URL, movieData, config)
+
+  return response.data
+}
+
+// Modificar película
+const updateMovie = async (movieData, token) => {
+  const { movieId, ...movieDataToUpdate } = movieData
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  const response = await axios.put(API_URL + movieId, movieDataToUpdate, config)
+
+  return response.data
+}
+
+// Eliminar película
+const deleteMovie = async (movieId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  const response = await axios.put(API_URL + movieId, config)
+
+  return response.data
+}
+
+const movieService = {
+  getAllGenres,
+  getAllMovies,
+  getContextMovies,
+  likeMovie,
+  dislikeMovie,
+  resetLikesMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie
 }
 
 export default movieService
--- movies-fullstack/src/Features/Movies/movieSlice.js
+++ movies-fullstack/src/Features/Movies/movieSlice.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import movieService from './movieService'
 
 const initialState = {
+  genres: [],
   movies: [],
   likedMovies: [],
   isLoading: false,
@@ -10,6 +11,97 @@ const initialState = {
   message: ''
 }
 
+// Obtener todos los géneros
+export const getAllGenres = createAsyncThunk('movies/get-genres', async (_, thunkAPI) => {
+  try {
+    return await movieService.getAllGenres()
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Obtener todas las películas (not logged)
+export const getAllMovies = createAsyncThunk('movies/get-movies', async (_, thunkAPI) => {
+  try {
+    return await movieService.getAllMovies()
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Obtener todas las películas (logged)
+export const getContextMovies = createAsyncThunk('movies/get-movies-context', async (_, thunkAPI) => {
+  try {
+    return await movieService.getContextMovies(thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Like movie
+export const likeMovie = createAsyncThunk('movies/like', async (movieId, thunkAPI) => {
+  try {
+    return await movieService.likeMovie(movieId, thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Disike movie
+export const dislikeMovie = createAsyncThunk('movies/dislike', async (movieId, thunkAPI) => {
+  try {
+    return await movieService.dislikeMovie(movieId, thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Reset likes/dislikes
+export const resetLikesMovie = createAsyncThunk('movies/reset-likes', async (movieId, thunkAPI) => {
+  try {
+    return await movieService.resetLikesMovie(movieId, thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Crear película
+export const createMovie = createAsyncThunk('movies/create', async (movieData, thunkAPI) => {
+  try {
+    return await movieService.createMovie(movieData, thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Modificar película
+// movieData debe incluir un campo movieId
+export const updateMovie = createAsyncThunk('movies/update', async (movieData, thunkAPI) => {
+  try {
+    return await movieService.updateMovie(movieData, thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Eliminar película
+export const deleteMovie = createAsyncThunk('movies/delete', async (movieId, thunkAPI) => {
+  try {
+    return await movieService.deleteMovie(movieId, thunkAPI.getState().auth.user.token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
 export const movieSlice = createSlice({
   name: 'movie',
   initialState,
@@ -17,6 +109,146 @@ export const movieSlice = createSlice({
     reset: (state) => initialState
   },
   extraReducers: (builder) => {
+    builder
+      .addCase(getAllGenres.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getAllGenres.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.genres = action.payload
+      })
+      .addCase(getAllGenres.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(getAllMovies.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getAllMovies.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = action.payload
+      })
+      .addCase(getAllMovies.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(getContextMovies.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getContextMovies.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = action.payload
+        state.likedMovies = action.payload.filter((movie) => movie.isLiked === 1)
+      })
+      .addCase(getContextMovies.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(likeMovie.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(likeMovie.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = state.movies.map((movie) => {
+          if (movie._id === action.payload._id) {
+            return action.payload
+          }
+          return movie
+        })
+        state.likedMovies.push(action.payload)
+      })
+      .addCase(likeMovie.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(dislikeMovie.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(dislikeMovie.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = state.movies.map((movie) => {
+          if (movie._id === action.payload._id) {
+            return action.payload
+          }
+          return movie
+        })
+        state.likedMovies = state.likedMovies.filter((movie) => movie._id !== action.payload._id)
+      })
+      .addCase(dislikeMovie.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(resetLikesMovie.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(resetLikesMovie.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = state.movies.map((movie) => {
+          if (movie._id === action.payload._id) {
+            return action.payload
+          }
+          return movie
+        })
+        state.likedMovies = state.likedMovies.filter((movie) => movie._id !== action.payload._id)
+      })
+      .addCase(resetLikesMovie.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(createMovie.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createMovie.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies.push(action.payload)
+      })
+      .addCase(createMovie.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(updateMovie.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(updateMovie.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = state.movies.map((movie) => {
+          if (movie._id === action.payload._id) {
+            return action.payload
+          }
+          return movie
+        })
+        state.likedMovies = state.likedMovies.map((movie) => {
+          if (movie._id === action.payload._id) {
+            return action.payload
+          }
+          return movie
+        })
+      })
+      .addCase(updateMovie.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
+      .addCase(deleteMovie.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(deleteMovie.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.movies = state.movies.filter((movie) => movie._id !== action.payload._id)
+        state.likedMovies = state.likedMovies.filter((movie) => movie._id !== action.payload._id)
+      })
+      .addCase(deleteMovie.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+      })
   }
 })
 
