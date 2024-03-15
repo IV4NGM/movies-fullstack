783f6ee41d68f368b3df45b1034c64bfa70a9fd0
IvanGM <ivangm_01@hotmail.com>
2024-03-15 14:40:58 -0600

    feat: Spinner añadido

---
+++ movies-fullstack/src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,13 @@
+import './Spinner.scss'
+
+const Spinner = () => {
+  return (
+    <div className='page-container'>
+      <div className='loadingSpinnerContainer'>
+        <div className='loadingSpinner' />
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
