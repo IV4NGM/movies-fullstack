370250bd343cf97ea3b9389498728b16604f2fa7
IvanGM <ivangm_01@hotmail.com>
2024-03-18 19:53:45 -0600

    feat: Modal component agregado

---
+++ movies-fullstack/src/Components/CustomModal/CustomModal.jsx
@@ -0,0 +1,46 @@
+import Modal from 'react-bootstrap/Modal'
+import './CustomModal.scss'
+
+const nothingFunction = () => {}
+
+// eslint-disable-next-line react/prop-types
+const CustomModal = ({ title, showModal, setShowModal, text, onYes = nothingFunction, onNo = nothingFunction, isCancelButton = true, textNo = 'No', textYes = 'Sí', estatico = false }) => {
+  return (
+    <Modal
+      show={showModal} backdrop={estatico ? 'static' : true} keyboard={!estatico} onHide={() => {
+        onNo()
+        setShowModal(false)
+      }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title><h3>{title}</h3></Modal.Title>
+      </Modal.Header>
+      <Modal.Body><p className='medium-text'>{text}</p></Modal.Body>
+      <Modal.Footer>
+        {
+          isCancelButton
+            ? (
+              <button
+                className='btn btn-outline-secondary btn-modal' onClick={() => {
+                  onNo()
+                  setShowModal(false)
+                }}
+              >{textNo}
+              </button>
+              )
+            : ''
+        }
+        <button
+          className='btn btn-outline-success btn-modal' onClick={() => {
+            onYes()
+            setShowModal(false)
+          }}
+        >
+          {textYes}
+        </button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default CustomModal
