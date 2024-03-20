657fe044f8125d7e057d84db23a727715a21f344
IvanGM <ivangm_01@hotmail.com>
2024-03-20 01:38:46 -0600

    feat: Agregados estilos de tarjetas de películas

--- movies-fullstack/src/Components/Movie/Movie.jsx
+++ movies-fullstack/src/Components/Movie/Movie.jsx
@@ -5,8 +5,10 @@ import { useSelector } from 'react-redux'
 import LikesButton from '@/Components/LikesButton/LikesButton'
 import NoMovie from '@/assets/NoMovie.jpg'
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import PlayArrowOutlinedIcon from '@mui/icons-material/PlayArrowOutlined'
 
 import StarImage from '@/assets/star-icon.webp'
+import { useState } from 'react'
 
 const Movie = ({ movieData }) => {
   const { user } = useSelector((state) => state.auth)
@@ -27,8 +29,50 @@ const Movie = ({ movieData }) => {
     }
     return route
   }
+
+  const [showDetails, setShowDetails] = useState(false)
+
   return (
-    <div className='card movie-card'>
+    // <div className='card movie-card'>
+    //   {user?.isAdmin
+    //     ? (
+    //       <div
+    //         className='edit-icon' onClick={(event) => {
+    //           event.stopPropagation()
+    //           navigate(`/edit/${movieData?._id}`)
+    //         }}
+    //       ><EditOutlinedIcon />
+    //       </div>
+    //     )
+    //     : ''}
+    //   <div className='card-body card-body-movie'>
+    //     <h5 className='card-title'>{movieData.title}</h5>
+    //     <img
+    //       src={movieData.poster_path || NoMovie} className='card-img-top card-img-movie' alt='Poster' onError={({ currentTarget }) => {
+    //         currentTarget.onerror = null
+    //         currentTarget.src = NoMovie
+    //       }}
+    //     />
+    //     <div className='movie-row-container'>
+    //       <p className='card-text bold-text'>Calificación:</p>
+    //       <p className='card-text'>{Math.round(movieData.vote_average * 10) / 10}</p>
+    //       <img className='star-image' src={StarImage} />
+    //     </div>
+    //     <div className='movie-row-container'>
+    //       <p className='card-text bold-text'>Estreno:</p>
+    //       <p className='card-text'>{dateWithoutTime}</p>
+    //     </div>
+    //     <div className='movie-genres-container'>
+    //       {movieData?.genres.map((genre, index) => <button className='btn btn-outline-secondary movie-genre' key={`movie-${movieData?._id}-genre-${index}`}>{genre.name}</button>)}
+    //     </div>
+    //     <div className='movie-row-container'>
+    //       <LikesButton like filled={movieData.isLiked === 1} likesCount={movieData.likes_count} movieId={movieData._id} />
+    //       <LikesButton like={false} filled={movieData.isLiked === -1} likesCount={movieData.dislikes_count} movieId={movieData._id} />
+    //     </div>
+    //     <Link to={isNotLoggedRedirect(`/movie/${movieData._id}`)} className='space-up'>Más información</Link>
+    //   </div>
+    // </div>
+    <div className={'movie-card' + (showDetails ? ' movie-card-shadow' : '')} onMouseEnter={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)} onClick={() => navigate(isNotLoggedRedirect(`/movie/${movieData._id}`))}>
       {user?.isAdmin
         ? (
           <div
@@ -40,31 +84,33 @@ const Movie = ({ movieData }) => {
           </div>
         )
         : ''}
-      <div className='card-body card-body-movie'>
-        <h5 className='card-title'>{movieData.title}</h5>
+      <img
+        src={movieData?.poster_path || NoMovie} className={'card-img-movie movie-card-element' + (!showDetails ? ' movie-card-element-visible' : ' movie-card-element-invisible')} alt='Poster' onError={({ currentTarget }) => {
+          currentTarget.onerror = null
+          currentTarget.src = NoMovie
+        }}
+      />
+      <div className={'movie-card-details' + (showDetails ? ' movie-card-element-visible' : ' movie-card-element-invisible')}>
         <img
-          src={movieData.poster_path || NoMovie} className='card-img-top card-img-movie' alt='Poster' onError={({ currentTarget }) => {
+          src={movieData?.backdrop_path || NoMovie} className='card-img-movie card-img-movie-backdrop' alt='Backdrop' onError={({ currentTarget }) => {
             currentTarget.onerror = null
             currentTarget.src = NoMovie
           }}
         />
+        <h5 className='movie-card-title'>{movieData.title}</h5>
         <div className='movie-row-container'>
-          <p className='card-text bold-text'>Calificación:</p>
-          <p className='card-text'>{Math.round(movieData.vote_average * 10) / 10}</p>
+          <p>Calificación:</p>
+          <p>{Math.round(movieData.vote_average * 10) / 10}</p>
           <img className='star-image' src={StarImage} />
         </div>
         <div className='movie-row-container'>
-          <p className='card-text bold-text'>Estreno:</p>
-          <p className='card-text'>{dateWithoutTime}</p>
+          <p>Estreno:</p>
+          <p>{dateWithoutTime}</p>
         </div>
         <div className='movie-genres-container'>
-          {movieData?.genres.map((genre, index) => <button className='btn btn-outline-secondary movie-genre' key={`movie-${movieData?._id}-genre-${index}`}>{genre.name}</button>)}
+          {movieData?.genres.map((genre, index) => <span className='movie-genre' key={`movie-${movieData?._id}-genre-${index}`}>{genre.name}</span>)}
         </div>
-        <div className='movie-row-container'>
-          <LikesButton like filled={movieData.isLiked === 1} likesCount={movieData.likes_count} movieId={movieData._id} />
-          <LikesButton like={false} filled={movieData.isLiked === -1} likesCount={movieData.dislikes_count} movieId={movieData._id} />
-        </div>
-        <Link to={isNotLoggedRedirect(`/movie/${movieData._id}`)} className='space-up'>Más información</Link>
+        <button className='btn btn-success movie-card-watch-now'><PlayArrowOutlinedIcon /> Ver ahora</button>
       </div>
     </div>
   )
