b11d6a4587d58b65548bf8879cf1d0ed7df456bc
IvanGM <ivangm_01@hotmail.com>
2024-03-20 23:47:44 -0600

    fix: Modificados estilos para móviles

--- movies-fullstack/src/main.jsx
+++ movies-fullstack/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { store } from '@/app/store.js'
 import { Provider } from 'react-redux'
 // Bootstrap CSS
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 // Bootstrap Bundle JS
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './index.scss'
