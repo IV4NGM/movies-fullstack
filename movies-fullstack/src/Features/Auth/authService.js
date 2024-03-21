a10e181a01947ea1efc61d2740774a290ccc770b
IvanGM <ivangm_01@hotmail.com>
2024-03-20 23:24:08 -0600

    feat: Añadido manejo de variables de entorno

--- movies-fullstack/src/Features/Auth/authService.js
+++ movies-fullstack/src/Features/Auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL + 'users/'
+const API_URL = process.env.REACT_APP_API_URL + 'users/'
 
 // Creamos la petición al backend para crear un usuario
 const register = async (userData) => {
